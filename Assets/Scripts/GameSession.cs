8a1be3d2fcb3f99593c4d0a506f8465092103264
thewhitesummer <fe_granger@hotmail.com>
2020-11-28 13:01:23 -0300

    initial commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {
+
+    [SerializeField] [Range(0.1f, 10f)] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoplayEnabled;
+
+    [SerializeField] int currentScore = 0;
+
+    private void Awake() {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if(gameStatusCount > 1) {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start() {
+        scoreText.text = currentScore.ToString();
+    }
+
+    void Update() {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore() {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame() {
+        Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled() {
+        return isAutoplayEnabled;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minXPos = 1f;
+    [SerializeField] float maxXPos = 15f;
+
+    Ball ball;
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start() {
+        ball = FindObjectOfType<Ball>();
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition() {
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(GetXPos(), minXPos, maxXPos);
+        transform.position = paddlePosition;
+    }
+
+    private float GetXPos() {
+        if(gameSession.IsAutoPlayEnabled()) {
+            return ball.transform.position.x;
+        }
+        else {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
